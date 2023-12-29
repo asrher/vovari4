37cb8eeaef926eb3830c685c13285392ba8edd2e
jeje <103472532+asrher@users.noreply.github.com>
2023-12-29 20:55:35 +0100

    Update Asms.js

--- commands/Asms.js
+++ commands/Asms.js
@@ -1,7 +1,7 @@
 const { cmd, sck1, sck, tlang, getAdmin, sleep  } = require('../lib');
-const { SMSActivate } = require('sms-activate-org');
+const { SMSActivate, SMSNumber } = require('sms-activate');
 
-/*
+
 const apiKey = 'e4cf5Ab978b562809cb7882f0bA8c24f'; // Replace 'myapikey' with your actual API key
 const api = new SMSActivate(apiKey);
 
@@ -25,41 +25,3 @@ cmd({
     await citel.reply("خطا");
   }
 });
-*/
-
-
-
-
-// Import necessary libraries
-const SMSActivateAPI = require('smsactivate-api'); // Make sure to install this library using npm or yarn
-
-// Initialize SMSActivate API with your API key
-const apiKey = 'e4cf5Ab978b562809cb7882f0bA8c24f'; // Replace 'myapikey' with your actual API key
-
-const sa = new SMSActivateAPI(apiKey);
-
-// Function to get a phone number
-cmd({
-  pattern: "getnumber",
-  desc: "Get a phone number",
-  category: "sms",
-  filename: __filename,
-},
-async (Void, citel) => {
-  try {
-    // Request a phone number for the 'vk' service, 'tele2' operator, country code 0, and for verification purposes
-    const number = await sa.getNumber({ service: 'vk', operator: 'tele2', country: 0, verification: true });
-    
-    if (number.phone) {
-      // If the phone number is retrieved successfully, send it in a message
-      return citel.reply(`Phone number: ${number.phone}`);
-    } else {
-      // If there's an error, print the error message
-      return citel.reply(`Error: ${number.message}`);
-    }
-  } catch (error) {
-    console.error(error);
-    return citel.reply('An error occurred while getting the phone number.');
-  }
-});
-
