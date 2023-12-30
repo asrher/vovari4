777cc0e7a228f358b5f9ca821304b3780ab312f4
jeje <103472532+asrher@users.noreply.github.com>
2023-12-30 18:09:27 +0100

    Update Asms.js

--- commands/Asms.js
+++ commands/Asms.js
@@ -1,27 +1,89 @@
-const { cmd, sck1, sck, tlang, getAdmin, sleep  } = require('../lib');
-const { SMSActivate, SMSNumber } = require('sms-activate-org');
+// Import necessary libraries and functions
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { PassThrough } = require('stream');
+const PDFDocument = require('pdfkit');
 
+// Function to search 3asq for manga information
+async function search3asq(q) {
+    try {
+        const { data } = await axios.get(`https://3asq.org/?s=${q}&post_type=wp-manga`);
+        const $ = cheerio.load(data);
 
-const apiKey = 'e4cf5Ab978b562809cb7882f0bA8c24f'; // Replace 'myapikey' with your actual API key
-const api = new SMSActivate(apiKey);
+        return $('.tab-summary').map((index, element) => ({
+            name: $(element).find('.post-title h3 a').text().trim(),
+            link: $(element).find('.post-title h3 a').attr('href'),
+            alternativeNames: $(element).find('.mg_alternative .summary-content').text().trim(),
+            genres: $(element).find('.mg_genres .summary-content a').map((i, el) => $(el).text()).get().join(', ')
+        })).get();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
 
+// Function to get all chapters of a manga
+async function getAllChapters(url) {
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
 
+        return $('.wp-manga-chapter').map((index, element) => ({
+            title: $(element).text().trim(),
+            link: $(element).find('a').attr('href'),
+            releaseDate: $(element).find('.chapter-release-date i').text().trim(),
+            views: $(element).find('.view').text().trim(),
+        })).get();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+// Function to generate a PDF of manga chapters
+async function getChapterPdf(url) {
+    // ... [Rest of the function remains the same]
+}
+
+// Command to search for manga information and fetch chapters
 cmd({
-  pattern: "nemra",
-}, async (Void, citel) => {
-  try {
-    const number = await api.getNumber({ service: 'Whatsapp', country: 'Philippines' });
-    await number.ready();
+    pattern: "manga",
+    desc: "Search for manga and fetch chapters",
+    category: "entertainment",
+    filename: __filename,
+},
+async (Void, citel, text) => {
+    if (!text) return citel.reply('Please provide a manga name.');
 
-    const phoneNumber = number.phoneNumber;
-    await citel.reply(`الرقم: ${phoneNumber}`);
+    try {
+        // Search for manga information
+        const mangaList = await search3asq(text);
+        
+        if (mangaList.length === 0) {
+            return citel.reply('No manga found.');
+        }
 
-    const code = await number.getCode(180);
-    await citel.reply(`الكود: ${code}`);
+        // Select the first manga from the list
+        const manga = mangaList[0];
+        
+        // Get all chapters of the selected manga
+        const chapters = await getAllChapters(manga.link);
 
-    await number.success();
-  } catch (error) {
-    console.error('خطا', error.message);
-    await citel.reply("خطا");
-  }
+        if (chapters.length === 0) {
+            return citel.reply('No chapters found for this manga.');
+        }
+
+        // Fetch the PDF of the first chapter
+        const pdfBuffer = await getChapterPdf(chapters[0].link);
+
+        if (!pdfBuffer) {
+            return citel.reply('Failed to generate PDF for the first chapter.');
+        }
+
+        // Send the PDF file
+        return Void.sendMessage(citel.chat, { document: { data: pdfBuffer, mimetype: 'application/pdf' } }, { quoted: citel });
+    } catch (error) {
+        console.error('Error:', error);
+        return citel.reply('An error occurred while fetching manga information.');
+    }
 });
