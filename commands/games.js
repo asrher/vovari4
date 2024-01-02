2c247dd10b5282f9d1d4d0db1a2734e0f9200da2
jeje <103472532+asrher@users.noreply.github.com>
2024-01-02 19:26:50 +0100

    Update games.js

--- commands/games.js
+++ commands/games.js
@@ -69,6 +69,10 @@ cmd({ on: "text" } , async (Void , citel) => {  // Use  async (Void , citel,text
   let id = citel.chat.split("@")[0] 
   let senderNum = citel.sender.split("@")[0];
 // ============== / Joinening people in game
+const targetUser = citel.mentionedJid ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : false;
+const registeredUser1 = await sck1.findOne({ id: targetUser });
+const registeredName1 = registeredUser ? registeredUser1.name : "دون لقب";
+
 const registeredUser = await sck1.findOne({ id: citel.sender });
 let registeredName = registeredUser ? registeredUser.name : "دون لقب";
 
@@ -107,7 +111,7 @@ else if(deathGame[id] && deathGame[id].start && deathGame[id].killer === citel.s
     var text = citel.text
     let num = parseInt(text) || false
   if(num && !isNaN(num) && deathGame[id].players[num] && deathGame[id].players[num]!==citel.sender  ){
-    await citel.reply(` ${registeredName} مطرود من  ${registeredName} !`,
+    await citel.reply(` @${deathGame[id].players[registeredName].split("@")[0]} مطرود من  ${registeredName} !`,
     {mentions:[citel.sender,deathGame[id].players[num]]})
 
 
