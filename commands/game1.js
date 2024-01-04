b46a92a25d14373ba6f5241b6af1535f3209d1a2
jeje <103472532+asrher@users.noreply.github.com>
2024-01-04 03:16:49 +0100

    Update game1.js

--- commands/game1.js
+++ commands/game1.js
@@ -36,9 +36,19 @@ cmd({
     return await citel.reply('No game in progress to stop!');
   }
 
+  let results = 'Game stopped. Here are the results:\n';
+
+  for (const participantId in wordGame[id].participants) {
+    const points = wordGame[id].participants[participantId];
+    const registeredUser = await sck1.findOne({ id: participantId });
+    const playerName = registeredUser ? registeredUser.name : "Unknown"; // Default name if user not found
+
+    results += `${playerName} (${participantId}) got ${points} points.\n`;
+  }
+
   wordGame[id].isActive = false;
 
-  return await citel.reply(getGameResults(id));
+  return await citel.reply(results);
 });
 
 cmd({ on: "text" }, async (Void, citel) => {
@@ -53,17 +63,9 @@ cmd({ on: "text" }, async (Void, citel) => {
 
     wordGame[id].participants[participantId]++;
 
-    await processGame(citel, id, participantId); // Call the function asynchronously
+    startGame(citel, id);
   }
 });
-
-async function processGame(citel, id, participantId) {
-  const registeredUser = await sck1.findOne({ id: participantId }); // Use await in an async function
-  const playerName = registeredUser ? registeredUser.name : "Unknown"; // Default name if user not found
-
-  startGame(citel, id); // Continue the game after processing
-}
-
 
 function startGame(citel, id) {
   const randomWords = ['ناروتو', 'تسونادي', 'لوفي', 'زورو', 'ناتسو', 'روميو', 'انديفار', 'كورابيكا']; // List of random words
@@ -71,18 +73,4 @@ function startGame(citel, id) {
   wordGame[id].currentWord = randomWords[randomIndex];
 
   citel.send(`${wordGame[id].currentWord}`);
-}
-
-function getGameResults(id) {
-  let results = 'Game stopped. Here are the results:\n';
-
-  for (const participantId in wordGame[id].participants) {
-    const points = wordGame[id].participants[participantId];
-    const registeredUser = await sck1.findOne({ id: participantId });
-    const playerName = registeredUser ? registeredUser.name : "Unknown"; // Default name if user not found
-
-    results += `${playerName} (${participantId}) got ${points} points.\n`;
-  }
-
-  return results;
 }
