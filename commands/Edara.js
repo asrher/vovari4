dfbf9696762032e1528090a1550c4f3b421275c8
jeje <103472532+asrher@users.noreply.github.com>
2024-01-05 01:10:02 +0100

    Update Edara.js

--- commands/Edara.js
+++ commands/Edara.js
@@ -2,30 +2,47 @@ const { cmd } = require('../lib');
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 const axios = require('axios');
+const Canvas = require('canvas');
+
+const createMeme = async (image) => {
+  return new Promise(async (resolve, reject => {
+    try {
+      const canvas = Canvas.createCanvas(347, 426);
+      const ctx = canvas.getContext(`2d`);
+      const image1 = await Canvas.loadImage(image);
+      const background = await Canvas.loadImage(`Siraj/meme/burn.png`);
+
+      ctx.drawImage(image1, 19, 31, 113, 154);
+      ctx.drawImage(background, 0, 0, 347, 426);
+
+      const buffer = canvas.toBuffer();
+      fs.writeFileSync(`./siraj/meme.png`, buffer);
+
+      resolve(`./siraj/meme.png`);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
 cmd({
-  pattern: 'meme',
-  category: 'image',
-}, async (Void, message) => {
+  pattern: 'ميم',
+  desc: '',
+  use: '<text>',
+  category: 'maker',
+  filename: __filename,
+},
+async (Void, citel, text) => {
+  if (!citel.quoted) return await citel.reply(`رد على صورة`);
+  if (citel.quoted.mtype != 'imageMessage') return await citel.reply("رد على صورة");
+
   try {
-    const text0 = 'Top Text';
-    const text1 = 'Bottom Text';
-
-    const response = await axios.post('https://api.imgflip.com/caption_image', {
-      template_id: "121580608", // ID of the "One Does Not Simply" meme template
-      text0: text0,
-      text1: text1,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-    });
-
-    const dataURL = response.data.data.url;
-
-    await Void.sendMessage(message.chat, { image: { url: dataURL } }, { quoted: message });
+    const media = await Void.downloadAndSaveMediaMessage(citel.quoted);
+    const memePath = await createMeme(media);
+    await Void.sendMessage(citel.chat, { image: { url: `./${memePath}` } }, { quoted: citel });
+    fs.unlinkSync(media);
+    fs.unlinkSync(memePath);
   } catch (e) {
-    await message.send(`خطأ`);
+    return await citel.send(`خطأ`);
   }
 });
